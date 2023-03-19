afe3105a2dc867fa11864535f90306583b2acce5
Denis <den.dvornikov.88@inbox.ru>
2023-03-19 16:49:20 +0600

    Выполнил задание 1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+System.Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите квадрат числа");
+int b = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+int c = a;
+while(i<=b){
+    c=c*a;
+    i++;
+}
+System.Console.WriteLine($"Результат {c}");
